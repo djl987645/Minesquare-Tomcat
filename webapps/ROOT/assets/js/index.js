70ad35e1d8f59fb8c0481738329a49300b6e558e
종민 박 <djl987645@kyonggi.ac.kr>
2024-05-14 06:09:55 +0900

    홈페이지 작성 준비

---
+++ webapps/ROOT/assets/js/index.js
@@ -0,0 +1,9 @@
+// TODO 자바스크립트 구현하기
+
+alert("hello world");
+setInterval(function () {
+  const currentBackground = document.body.style.backgroundImage;
+  const currentIndex = parseInt(currentBackground.match(/(\d+)\.jpg/)[1]);
+  const nextIndex = (currentIndex % 5) + 1;
+  document.body.style.backgroundImage = `url("assets/background/${nextIndex}.jpg")`;
+}, 5000);
